5c2cc207ef0e9ace2f847f0cc66ec773e63ef0c6
Steven Leibrock <steven.leibrock@gmail.com>
2015-09-01 17:22:50 -0400

    Adding Highest Divisible Triangle number

---
+++ TriangleDivisible.rkt
@@ -0,0 +1,27 @@
+#lang racket
+; First triangle number with 500 divisors
+; Triangle x = sum [1 .. x]
+
+; Find all factors of x
+; Calculates numbers from 1..sqrt(x)
+(define (find-factors x acc cont)
+    (if (= acc (add1 (floor (sqrt x))))
+      cont
+      (find-factors x (add1 acc)
+        (if (= 0 (modulo x acc))
+          (append (list acc (floor (/ x acc))) cont)
+          cont))))
+
+; Function to loop until we find a number with 500 or more divisors
+(define (find-highest n sum)
+  (let* ((nsum (+ sum n))
+        (fact (find-factors nsum 1 '())))
+    (if (<= 500 (length fact))
+      nsum
+      (find-highest (add1 n) nsum))))
+
+(define (main)
+  (display "Triangle num with 500 divisors:: ")
+  (displayln (find-highest 1 0)))
+
+(time (main))
