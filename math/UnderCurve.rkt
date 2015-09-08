f9cb3659bde8a83531cad6aebf05946df160d1f7
Steven Leibrock <steven.leibrock@gmail.com>
2015-09-08 13:25:53 -0400

    Adding a calculus program

---
+++ math/UnderCurve.rkt
@@ -0,0 +1,23 @@
+#lang racket
+; Area Under a Curve using Calculus and iterative approach
+; Riemman Sum = [ f(x) * dx | x = [a..b] ]
+
+; Procedure -> Real -> Real -> Real -> Real -> Real
+(define (under-curve fx dx a b acc)
+  (if (>= a b)
+      acc
+      (under-curve fx dx (+ a dx) b
+                   (+ acc (* dx (fx a))))))
+
+(define (square x) (* x x))
+(define (ad-square x) (/ (* x x x) 3))
+
+(define (under-curve-ad a b)
+  (- (ad-square b) (ad-square a)))
+
+(define (main)
+  (display "Iterative: ")
+  (displayln (under-curve square 0.1 1 10 0))
+  (display "Mathematical: ")
+  (displayln (under-curve-ad 1 10)))
+
